4258802cd1d75bb77e6844f7cff2b678a48f3ddd
Malinda <wmdmalinda@gmail.com>
2024-07-24 20:57:00 +0530

    Update main.go

--- main.go
+++ main.go
@@ -11,33 +11,33 @@ func main() {
 	bc := internal.NewBlockchain()
 	defer bc.DB.Close()
 
-	from := "1F24YsuVtXTz6e5d3FvAjxef2d1AbPqVYAoKDhxEiZGdiLxXM5BUKPsN4ZhzcvAvQi7e8XMq"
-	to := "1R5Xq1eGS46yPb7c5GDgGsABAWMkZFs9QDYKM2c84GS2DcLAfEbnbJimtRkbJ87WTYobFMGm"
+	from := "1PBa8u8mHGHnz1ksCRLX7NsmZTj2uq5htsWTiqoWKU3SRJPiKzQeiH4KL6HmLVp5JDsjymDU"
+	to := "1MZurNJ3htDMv4WUxZmbg3cUrkBXRn67XkoXdaC1M7Qp7DcpJBGX6icR58mvt9xZKsgEgpJ8"
 
 	// // 1st time only
-	// ctx := bc.NewCoinbaseTransaction(addr1Pubkeyhash, addr1.PublicKey)
+	// ctx := bc.NewCoinbaseTransaction(from)
 	// bc.AddBlock([]*internal.Transaction{ctx})
 
-	fmt.Printf("\n%s balance: %d\n", from, bc.GetBalance(from))
+	fmt.Printf("\n%s balance: %d\n\n", from, bc.GetBalance(from))
 	fmt.Printf("%s balance: %d\n\n", to, bc.GetBalance(to))
 
 	// FROM 1 to 2
-	tx, err := bc.NewTransaction(from, to, 2)
-	if err != nil {
-		log.Println("TX:", err)
-	}
-	if tx != nil {
-		bc.AddBlock([]*internal.Transaction{tx})
-	}
-
-	// // FROM 2 to 1
-	// tx, err = bc.NewTransaction(to, from, 3)
+	// tx, err := bc.NewTransaction(from, to, 2)
 	// if err != nil {
 	// 	log.Println("TX:", err)
 	// }
 	// if tx != nil {
 	// 	bc.AddBlock([]*internal.Transaction{tx})
 	// }
+
+	// // FROM 2 to 1
+	tx, err := bc.NewTransaction(to, from, 2)
+	if err != nil {
+		log.Println("TX:", err)
+	}
+	if tx != nil {
+		bc.AddBlock([]*internal.Transaction{tx})
+	}
 
 	// TODO:
 	// Clean up transactions.go
